f00c4e4c943e943e0d76c774fcd4fc11842810e9
kovaly11 <kovaly125@gmail.com>
2021-04-08 22:05:46 +0200

    First version LAB2

---
+++ LAB 2/LAB 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace LAB_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Dzialanie klasy Licz
+            Licz piec = new Licz(5);
+            piec.PokazStan();
+            piec.Dodaj(3);
+            piec.PokazStan();
+            piec.Odejmij(2);
+            piec.PokazStan();
+            Console.WriteLine();
+
+            // Dzialanie klasy Sumator
+            Sumator sumat = new Sumator(new List<int> {0,1,2,3,4,5,6,7,8,9,10});
+            Console.WriteLine(sumat.Suma());
+            Console.WriteLine(sumat.SumaPodziel3());
+            Console.WriteLine(sumat.IleElementow());
+            sumat.PokazLiczby();
+            sumat.PokazLiczbyOdDo(-1, 4);
+            sumat.PokazLiczbyOdDo(5, 17);
+            sumat.PokazLiczbyOdDo(8, 3);
+            Console.WriteLine();
+
+            //Dzialanie klasy Data
+            Data data = new Data();
+            data.WczyajDatuTeraz();
+            data.PokazDatuWFormacie("dd-MM-yyyy ");
+            data.DodajTydzien();
+            data.PokazDatuWFormacie("dd-MM-yyyy ");
+            data.OdejmijTydzien();
+            data.PokazDatuWFormacie("dd-MM-yyyy ");
+        }
+    }
+}
---
+++ LAB 2/LAB 2/Sumator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_2
+{
+    class Sumator
+    {
+        private List<int> Liczby = new List<int>();
+
+        public Sumator() { }
+        public Sumator(List<int> tab) 
+        {
+            Liczby = tab;
+        }
+        public int Suma()
+        {
+            int sum=0;
+            foreach(int liczba in Liczby)
+            {
+                sum += liczba;
+            }
+            return sum;
+        }
+        public double SumaPodziel3()
+        {
+            return Suma() / 3.0;
+        }
+        public int IleElementow()
+        {
+            return Liczby.Count;
+        }
+        public void PokazLiczby()
+        {
+            foreach (int liczba in Liczby)
+            {
+                Console.Write(liczba + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void PokazLiczbyOdDo(int lowIndex, int highIndex)
+        {
+            int temp;
+            if (lowIndex < 0)
+                lowIndex = 0;
+            if (highIndex < 0)
+                highIndex = 0;
+            if (lowIndex >= Liczby.Count)
+                lowIndex = Liczby.Count - 1;
+            if (highIndex >= Liczby.Count)
+                highIndex = Liczby.Count - 1;
+            if (highIndex < lowIndex) 
+            {
+                temp = highIndex;
+                highIndex = lowIndex;
+                lowIndex = temp;
+            }
+            for (int i = lowIndex; i <= highIndex; i++)
+                Console.Write(Liczby[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
