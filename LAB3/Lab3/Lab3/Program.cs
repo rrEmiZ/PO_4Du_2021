4d25d5d67795c8e4e0e6e10f1a825d5892df7dc4
LewMatt <lewmateush@gmail.com>
2021-04-24 20:05:39 +0200

    Files updated.

--- LAB3/Lab3/Lab3/Program.cs
+++ LAB3/Lab3/Lab3/Program.cs
@@ -3,78 +3,204 @@ using System.Collections.Generic;
 
 namespace Lab3
 {
+    //zad1
+
     public class Shape
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Width { get; set; }
-
-        public int Height { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
 
         public virtual void Draw()
         {
-            for (int i = 0; i < Y + Height; i++)
-            {
-                if (i < Y)
-                {
-                    Console.WriteLine();
-                    continue;
-                }
-
-                for (int j = 0; j < X + Width; j++)
-                {
-                    if (j < X)
-                    {
-                        Console.Write(" ");
-                        continue;
-                    }
-
-                    Console.Write("*");
-
-                }
-
-                Console.WriteLine();
-
-
-            }
-
-
 
         }
 
     }
 
+    public class Rectangle:Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rectangle");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Triangle");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Circle");
+        }
+    }
+
+    //zad2
+
+    public class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public string pesel;
+
+        public Osoba(string imie, string nazwisko, string pesel)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.pesel = pesel;
+        }
+
+        public void SetFirstName(string x)
+        {
+            this.imie = x;
+        }
+
+        public void SetLastName(string x)
+        {
+            this.nazwisko = x;
+        }
+
+        public void SetPesel(string x)
+        {
+            this.pesel = x;
+        }
+
+        public string GetGender()
+        {
+            string gender = "";
+            if(pesel[9] % 2 == 0)
+            {
+                gender = "kobieta";
+            }
+            else
+            {
+                gender = "mezczyzna";
+            }
+            return gender;
+        }
+
+        public virtual void GetEducationInfo() { }
+        public virtual void GetFullName() { }
+        public virtual void CanGoAloneToHome() { }
 
 
+    }
 
+    public class Uczen : Osoba
+    {
+        public string szkola;
+        public bool MozeSamWracacDoDomu;
 
+        public Uczen(string imie, string nazwisko, string pesel, string szkola, bool czymozesamwracac) : base(imie, nazwisko, pesel)
+        {
+            this.szkola = szkola;
+            this.MozeSamWracacDoDomu = czymozesamwracac;
+        }
+
+        public void SetSchool(string x)
+        {
+            this.szkola = x;
+        }
+
+        public void ChangeSchool(string x)
+        {
+            this.szkola = x;
+        }
+
+        public void SetCanGoHomeAlone(bool x)
+        {
+            this.MozeSamWracacDoDomu = x;
+        }
+
+        public override void GetEducationInfo()
+        {
+            Console.WriteLine("Imie:" + this.imie);
+            Console.WriteLine("Nazwisko:" + this.nazwisko);
+            Console.WriteLine("Pesel:" + this.pesel);
+            Console.WriteLine("Szkola:" + this.szkola);
+        }
+
+        public override void GetFullName()
+        {
+            Console.WriteLine(this.imie + " " + this.nazwisko);
+        }
+
+        public override void CanGoAloneToHome()
+        {
+            if(this.MozeSamWracacDoDomu == true)
+            {
+                Console.WriteLine("Moze sam wracac do domu.");
+            }
+            else
+            {
+                Console.WriteLine("Nie moze sam wracac do domu.");
+            }
+        }
+
+    }
+
+    public class Nauczyciel : Uczen
+    {
+        public string TytulNaukowy;
+        public List<Uczen> PodwladniUczniowie = new List<Uczen>();
+
+        public Nauczyciel(string imie, string nazwisko, string pesel, string szkola, bool czymozesamwracac,string tytul):base(imie,nazwisko,pesel,szkola,czymozesamwracac)
+        {
+            this.TytulNaukowy = tytul;
+        }
+
+        public void DodajUczniow()
+        {
+            PodwladniUczniowie.Add(new Uczen("Adam", "Kot", "09231367175", "Szkola podstawowa nr 1", true));
+            PodwladniUczniowie.Add(new Uczen("Jan", "Kowal", "08291743677", "Szkola podstawowa nr 1", false));
+            PodwladniUczniowie.Add(new Uczen("Maria", "Lew", "06322972823", "Szkola podstawowa nr 1", true));
+        }
+
+        public void WhichStudentCanGoHomeAlone()
+        {
+            foreach(var x in PodwladniUczniowie)
+            {
+                if (x.MozeSamWracacDoDomu == true)
+                {
+                    x.GetFullName();
+                }
+            }
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            List<Shape> list = new List<Shape>();
+            //zad1
 
-            list.Add(new Shape()
-            {
-                X = 3,
-                Y = 4,
-                Height = 2,
-                 Width = 6
+            //List<Shape> shapes = new List<Shape>();
 
-            }
-            );
+            //shapes.Add(new Rectangle());
+            //shapes.Add(new Triangle());
+            //shapes.Add(new Circle());
 
-            //list.Add(new Rectangle());
+            //foreach(var x in shapes)
+            //{
+            //    x.Draw();
+            //}
 
+            //zad2
 
-            foreach (var item in list)
-            {
-                item.Draw();
-            }
+            Nauczyciel nauczyciel1 = new Nauczyciel("Jan", "Kowalski", "88031296717", "Liceum nr 2", true, "Inzynier");
 
+            nauczyciel1.DodajUczniow();
 
-            Console.ReadLine();
+            nauczyciel1.WhichStudentCanGoHomeAlone();
+
         }
     }
 }
