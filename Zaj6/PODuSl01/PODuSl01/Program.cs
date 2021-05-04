afcd844678e9319b0fb4a6c8c0aa7088e6aa25dd
Bartekciolek@33 <bartekciolek33@gmail.com>
2021-05-04 19:39:09 +0200

    zad4

--- Zaj6/PODuSl01/PODuSl01/Program.cs
+++ Zaj6/PODuSl01/PODuSl01/Program.cs
@@ -72,6 +72,80 @@ namespace PODuSl01
     }
 
 
+    public interface IReppository<T>
+    {
+        public List<T> GetAll();
+    }
+
+    public class RootRepository: IReppository<Root>
+    {
+        List<Root> objects;
+        public RootRepository()
+        {
+
+            using (var sr = new StreamReader("db.json"))
+            {
+                var json = sr.ReadToEnd();
+
+                objects = JsonConvert.DeserializeObject<List<Root>>(json);
+            }
+        }
+
+        public List<Root> GetAll()
+        {
+            return objects;
+        }
+
+        public Root? FindOneByYearAndCountryName(string year, string country)
+        {
+            foreach(Root r in objects)
+            {
+                if (r.date == year && r.country.value == country)
+                {
+                    return r;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class RootService
+    {
+        RootRepository rootRepository;
+
+        public RootService()
+        {
+            rootRepository = new RootRepository();
+        }
+
+        public int GetPopulationDifference(string countryName, string fromDate, string toDate)
+        {
+            return GetPopulationByYearAndCountry(countryName, toDate) - GetPopulationByYearAndCountry(countryName, fromDate);
+        }
+
+        public int GetPopulationByYearAndCountry(string countryName, string date)
+        {
+            Root root = rootRepository.FindOneByYearAndCountryName(date, countryName);
+            if (root == null)
+            {
+                throw new InvalidDataException("Nie znaleziono danych");
+            }
+            return Int32.Parse(root.value);
+        }
+
+        public double GetPercentagePopulationDifference(string countryName, string date)
+        {
+            int populationTo = GetPopulationByYearAndCountry(countryName, date);
+
+            string fromDate = (Int32.Parse(date) - 1).ToString();
+            int populationFrom = GetPopulationByYearAndCountry(countryName, fromDate);
+
+            return ((populationTo - populationFrom) / Convert.ToDouble(populationFrom)) * 100;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
@@ -137,6 +211,69 @@ namespace PODuSl01
                 }
             }
             Console.WriteLine($"Liczba kobiet w pliku wynosi: {femalesNum}");
+
+
+            //------------ZAD4----------------
+            var rootService = new RootService();
+            try
+            {
+                Console.WriteLine("Różnica populacji w Indiach między rokiem 1970 i 2000:");
+                Console.WriteLine(rootService.GetPopulationDifference("India", "1970", "2000"));
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine($"Wystąpił błąd: {e.Message}");
+            }
+
+            try
+            {
+                Console.WriteLine("Różnica populacji w USA między rokiem 1965 i 2010:");
+                Console.WriteLine(rootService.GetPopulationDifference("United States", "1965", "2010"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Wystąpił błąd: {e.Message}");
+            }
+
+            try
+            {
+                Console.WriteLine("Różnica populacji w Chianch między rokiem 1980 i 2018:");
+                Console.WriteLine(rootService.GetPopulationDifference("China", "1980", "2018"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Wystąpił błąd: {e.Message}");
+            }
+
+            try
+            {
+                Console.WriteLine("Podaj nazwę kraju:");
+                string country = Console.ReadLine();
+
+                Console.WriteLine("Podaj datę 1:");
+                string dateFrom = Console.ReadLine();
+
+                Console.WriteLine("Podaj datę 2:");
+                string dateTo = Console.ReadLine();
+
+
+                Console.WriteLine("Różnica populacji dal wybranego kraju: " + rootService.GetPopulationDifference(country, dateFrom, dateTo));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Wystąpił błąd: {e.Message}");
+            }
+
+            try
+            {
+                Console.WriteLine("Wstrost procentowy populacji w Indiach w roku 1993:");
+                Console.WriteLine(rootService.GetPercentagePopulationDifference("India", "1993") + "%");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Wystąpił błąd: {e.Message}");
+            }
+
 
             Console.ReadLine();
 
