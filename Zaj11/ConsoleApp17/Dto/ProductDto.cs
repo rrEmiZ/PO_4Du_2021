c496938576c30e32b042d2f709c627aa0d7f0d40
Jakub Jońca <jakubjonca@outlook.com>
2021-05-20 14:13:00 +0200

    EF Net Framework

---
+++ Zaj11/ConsoleApp17/Dto/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17.Dto
+{
+    public class ProductDto
+    {
+        public int Id { get;  set; }
+        public string Name { get;  set; }
+        public string BrandName { get;  set; }
+        public int BrandId { get;  set; }
+        public string CategoryName { get;  set; }
+        public int CategoryId { get;  set; }
+    }
+}
---
+++ Zaj11/ConsoleApp17/Program.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp17.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        public static List<ProductDto> GetProductDtos()
+        {
+            try
+            {
+                using (var ctx = new BikeStoresEntities())
+                {
+                    return ctx.products.Select(x => new ProductDto()
+                    {
+                        Id = x.product_id,
+                        Name = x.product_name,
+                        BrandName = x.brand.brand_name,
+                        BrandId = x.brand_id,
+                        CategoryName =  x.category.category_name,
+                        CategoryId = x.category_id
+                    }).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var ctx = new BikeStoresEntities();
+            //var productObj = ctx.products.FirstOrDefault();
+
+            //ctx.products.Remove(productObj);
+
+            //ctx.SaveChanges();
+
+            //              var productDtos = GetProductDtos();
+
+            var orders = ctx.orders.FirstOrDefault();
+
+
+            orders.
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
