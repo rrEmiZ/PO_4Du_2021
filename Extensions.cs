9662bf69c064d22257026dd0418e7688d59936b0
MaciejPelc <56410188+MaciejPelc@users.noreply.github.com>
2021-06-23 12:28:54 +0200

    zadania

---
+++ Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab13
+{
+    public static class Extensions
+    {
+        public static void PrintProbs(this object obj)
+        {
+            var type = obj.GetType();
+
+            var props = type.GetProperties();
+
+            foreach (var prop in props)
+            {
+                Console.WriteLine(prop.Name + " - " + prop.GetValue(obj));
+            }
+        }
+    }
+}
