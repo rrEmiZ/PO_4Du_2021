faaf911d2227fbdc99b78221487e6bf8e6e20f48
Weronika <werma56s@gmail.com>
2021-06-23 15:59:16 +0200

    LAB15-16

---
+++ Zaj18/Zaj18/ApiCovid/Premium.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zaj18.ApiCovid
+{
+    public class Premium
+    {
+    }
+}
---
+++ Zaj18/Zaj18/ApiCovid/Root.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zaj18.ApiCovid
+{
+    class Root
+    {
+        public string ID { get; set; }
+        public string Message { get; set; }
+        public Global Global { get; set; }
+        public List<Countries> Countries { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
